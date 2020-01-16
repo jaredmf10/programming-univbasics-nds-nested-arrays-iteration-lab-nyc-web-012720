092c4c6363b8784d4ec32395b32a6fd03aadae4b
Jared Fleming <jaredmf10@gmail.com>
2020-01-16 18:35:45 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -27,7 +27,7 @@ my_favs_string = ' ';
 
 src.each do |sub_array|
 my_temp_string = "I love #{src[0]} and #{src[1]} on my pizza"
-my_return_array << my_temp_string
+my_fav_array << my_temp_string
 end
 end
 def find_greater_pair(src)
